0f8b3c81be131e9f919890fbf3f86bdc59070c03
dacvs <david.a.c.v.smith@gmail.com>
2024-04-27 07:01:20 -0700

    quadratic remark

--- 06/d06p2.py
+++ 06/d06p2.py
@@ -11,3 +11,16 @@ with open("input.txt") as f:
         if dist > d:
             ans += 1
     print("ans", ans)
+
+    # Let x = holdtime.
+    # We are counting integer values of x in interval [0, t) where d < (t - x) * x.
+    # That is, where x * x - t * x + d < 0.
+    # The left side of the inequality is a quadratic polynomial (in x). The count is
+    # floor(R) - ceil(r) + 1 where r and R (with r <= R) are the roots of the polynomial.
+
+    if False:
+        import math
+        D = t * t - 4 * d
+        R = (t + math.sqrt(D)) / 2
+        r = (t - math.sqrt(D)) / 2
+        print("ans", math.floor(R) - math.ceil(r) + 1)

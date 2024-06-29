7dd7afeb7fec32eb1332c7c70ab8c35ca1fe7853
dacvs <david.a.c.v.smith@gmail.com>
2024-06-29 08:09:40 -0700

    tidy day 9

---
+++ d09p1.py
@@ -0,0 +1,10 @@
+import lib
+import d09
+import numpy as np
+ans = 0
+for s in lib.block("input/09.txt"):
+    As = [np.array([int(n) for n in s.split()])]
+    while not np.all(0 == As[-1]):
+        As.append(d09.diff(As[-1]))
+    ans += sum(A[-1] for A in As)
+print("ans", ans)
---
+++ d09p2.py
@@ -0,0 +1,10 @@
+import lib
+import d09
+import numpy as np
+ans = 0
+for s in lib.block("input/09.txt"):
+    As = [np.array([int(n) for n in s.split()])]
+    while not np.all(0 == As[-1]):
+        As.append(d09.diff(As[-1]))
+    ans += sum((-1)**i * A[0] for i, A in enumerate(As))
+print("ans", ans)
